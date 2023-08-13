fceac760510be3a3b6330992d8f97728bffa831f
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-13 11:52:34 +0500

    Fcking huligans

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int larry = 4-1;
+        int harry = 7-1;
+        System.out.println("Я тебе расскажу сколько банок не прострелили Гарри и Ларри. Гарри не прострелил:" + harry + " а Ларри:" + larry );
     }
 }
